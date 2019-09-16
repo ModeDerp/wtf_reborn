bc365092f06adfd3c1e4fca9b8b9696379c055aa
Daniel <Daniel.kull@elev.ga.ntig.se>
2019-09-16 16:03:53 +0200

    Added page for specific group

---
+++ lib/pluggy/controllers/group_controller.ex
@@ -0,0 +1,22 @@
+defmodule Pluggy.GroupController do
+  require IEx
+
+  alias Pluggy.Student
+  alias Pluggy.Group
+  alias Pluggy.User
+  import Pluggy.Template, only: [render: 2, srender: 2]
+  import Plug.Conn, only: [send_resp: 3]
+
+  def index(conn, params) do
+    # get user if logged in
+    session_user = conn.private.plug_session["user_id"]
+
+    current_user =
+      case session_user do
+        nil -> nil
+        _ -> User.get(session_user)
+      end
+
+    send_resp(conn, 200, srender("students/group", group: Group.get(params["id"]), students: [%{id: 10, first_name: "Daniel", last_name: "Kull"}])) #srender("students/group", group: Group.get(params["id"]), user: current_user, students: Group.get_students(params["id"])))
+  end
+end
---
+++ lib/pluggy/models/group.ex
@@ -0,0 +1,16 @@
+defmodule Pluggy.Group do
+  defstruct(id: nil, school_id: nil, name: "", img: "", about: "")
+
+  alias Pluggy.Group
+
+  def get(id) do
+    Postgrex.query!(DB, "SELECT * FROM groups WHERE id = $1 LIMIT 1", [id],
+      pool: DBConnection.Poolboy
+    ).rows
+    |> to_struct
+  end
+
+  def to_struct([[id, school_id, name, img, about]]) do
+    %Group{id: id, school_id: school_id, name: name, img: img, about: about}
+  end
+end
