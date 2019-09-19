e4e07e2bca84baad9f84865e13defac41eab17af
hugo <hugo.engstromsvanberg@elev.ga.ntig.se>
2019-09-19 10:10:34 +0200

    Added function for getting all teachers

--- lib/pluggy/models/user.ex
+++ lib/pluggy/models/user.ex
@@ -10,7 +10,18 @@ defmodule Pluggy.User do
     |> to_struct
   end
 
+  def get_teachers() do
+    Postgrex.query!(DB, "SELECT id, username, permissions FROM users WHERE permissions != 0", [],
+      pool: DBConnection.Poolboy
+    ).rows
+    |> to_struct_list
+  end
+
   def to_struct([[id, username, permissions]]) do
     %User{id: id, username: username, permissions: permissions}
   end
+
+  def to_struct_list(_, acc \\ [])
+  def to_struct_list([], acc), do: Enum.reverse(acc)
+  def to_struct_list([head | tail], acc), do: to_struct_list(tail, [to_struct([head]) | acc])
 end
