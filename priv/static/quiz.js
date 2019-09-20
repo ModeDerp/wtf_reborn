57363b9f99a36e7c8f6ba3b1ee877e24a2a77bff
Mattis Abrahamsson <modederp@hotmail.com>
2019-09-20 13:43:17 +0200

    Final fixes to quiz

--- priv/static/quiz.js
+++ priv/static/quiz.js
@@ -1,43 +1,53 @@
 let students = [];
 let availableGuesses = [];
-let length = 0;
 let counter = 0;
-let currentGuess = {};
-let guessed = true;
-let correct = 0;
+let correctObject = {};
+let hasGuessed = true;
+let score = 0;
 
 const qs = (el) => {
     return document.querySelector(el);
 }
 
+const qsa = (el) => {
+    return document.querySelectorAll(el);
+}
+
 fetch("/json/"+id).then(res => {
     res.json().then(json => {
-        students = json;
-        length = students.length;
-
-        console.log(students);
-
+        students = shuffle(json);
         nextGuess();
     })
 });
 
+const generateChoices = () => {
+    let arr = [];
+    Object.assign(arr, students);
+    temp = arr.splice(counter,1);
+    arr.length = 5;
+    arr.push(temp[0]);
+    availableGuesses = shuffle(arr);
+}
+
 const nextGuess = () => {
-    if(counter < length){
-        if(guessed){
-            Object.assign(availableGuesses, students)
-            currentGuess = students[counter];
-            guessed = false;
+    updateCounter();
+    if(hasGuessed){
+        if(counter < students.length){
+            generateChoices();
+
+            correctObject = students[counter];
+            hasGuessed = false;
     
             appendAnswers();
             appendImage();
     
             counter++;
-        } else {
-            console.log("You haven't guessed yet");
-        }
-    } else{
-        appendResults();
-    } 
+        } else{
+            appendResults();
+        } 
+    } else {
+        console.log("You haven't guessed yet");
+    }
 }
 
 const appendImage = () => {
@@ -47,32 +57,41 @@ const appendImage = () => {
 const appendAnswers = () => {
     qs(".quiz-answers").innerHTML = "";
     for(guess of shuffle(availableGuesses)){
-        qs(".quiz-answers").insertAdjacentHTML("beforeend", `<div class="quiz-answer" onclick="handleGuess(this, ${guess.id})"><span>${guess.first_name + " " + guess.last_name}</span></div>`)
+        qs(".quiz-answers").insertAdjacentHTML("beforeend", `<div id="guess_${guess.id}" class="quiz-answer" onclick="handleGuess(this, ${guess.id})"><span>${guess.first_name + " " + guess.last_name}</span></div>`)
     }
 }
 
 const handleGuess = (el, id) => {
-    if(!guessed){
-        guessed = true;
-        if(id == currentGuess.id) correctGuess(el);
+    if(!hasGuessed){
+        hasGuessed = true;
+        if(id == correctObject.id) correctGuess(el);
         else incorrectGuess(el);
     } else {
         console.log("You have already made your guess!");
     }
 }
 
-correctGuess = el => {
-    correct += 1;
+const correctGuess = el => {
+    score += 1;
     el.classList.add("correct");
 }
 
-incorrectGuess = el => {
+const incorrectGuess = el => {
     el.classList.add("incorrect");
+    obj = availableGuesses.find(el => {
+        return el == correctObject
+    });
+    qs(`#guess_${obj.id}`).classList.add("blue");
 }
 
-appendResults = () => {
+const appendResults = () => {
     qs(".quiz-container").innerHTML = "";
-    qs(".quiz-container").insertAdjacentHTML("beforeend", `<div class="title"><h1>Du hade ${correct + " / " + length} rätt!</h1></div>`)
+    qs(".quiz-container").insertAdjacentHTML("beforeend", `<div class="title"><h1>Du hade ${score + " / " + students.length} rätt!</h1></div>`)
+}
+
+const updateCounter = () => {
+    qs(".quiz-counter").innerHTML = "";
+    qs(".quiz-counter").insertAdjacentHTML("beforeend", `<p>${counter+1 + " / " + students.length}</p>`)
 }
 
 const shuffle = (a) => {
